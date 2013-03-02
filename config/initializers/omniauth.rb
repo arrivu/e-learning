8aec1c5e1fb1d50f419af194882d5dfd7707a853
samuelrails <samuels410@gmail.com>
2013-03-02 15:59:39 +0530

    google social login add

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # The following is for facebook
   # provider :facebook,  "312875045482031", '650ffb56571917b1b0c08200bf85677c', {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
- provider :facebook, '312875045482031', '650ffb56571917b1b0c08200bf85677c'
+  # Local Host
+  provider :facebook, '586163688080266', '093ab151dc5385a434b0e03815ac851f'
   provider :linkedin, '31azcdigi8tt', "OKd1hmuJoNOg9J2g"
   provider :google_oauth2, '928801233183', '-nMQ0dEfcaBITxzwYHZPQ3v-'
-  # If you want to also configure for additional login services, they would be configured here.
+
+#heroku
+  # provider :facebook, '	151537311672530', 'ed4a4725a9ff31f3ead5d201ddcb7476'
+  # provider :linkedin, 'gl1h6zyltlem', "wIfucZImlYGqFO3r"
+  # provider :google_oauth2, '928801233183-dg701idms6tr6907bk6m2i8boishu4io.apps.googleusercontent.com', 'n61XG48z1DMc6kGiY6zCuiUM'
+  # # If you want to also configure for additional login services, they would be configured here.
 end
