8438499b880524968d7f4a16931be3b73f28a84e
samuelrails <samuels410@gmail.com>
2013-03-02 14:13:46 +0530

    Integrate with google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook,  "312875045482031", '650ffb56571917b1b0c08200bf85677c', {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
  provider :facebook, '312875045482031', '650ffb56571917b1b0c08200bf85677c'
   provider :linkedin, '31azcdigi8tt', "OKd1hmuJoNOg9J2g"
+  provider :google_oauth2, '928801233183', '-nMQ0dEfcaBITxzwYHZPQ3v-'
   # If you want to also configure for additional login services, they would be configured here.
 end
